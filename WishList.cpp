2089d8dd5bc1bb42c60f32301f26cc3d7d12a86a
Resizt <resiztdzn@gmail.com>
2019-10-18 12:49:36 -0400

    Added most of the functionalities
    The add family feature is done, along with the delete family member and varioud listing. An update to the terminal gui so its more pleasing to look at and added a new function "System("CLR")" in void cleanup to clean up the terminal after almost everyuse for a clean layout.
    
    Im looking for a better way to make the gui look more appeling in the terminal.

--- WishList.cpp
+++ WishList.cpp
@@ -11,23 +11,68 @@ using namespace std;
 void welcome();
 void menu(int&, string[], int&);
 void header(string[]);
+void cleanup();
 
 // The Family functions
 void addfamily(string[], int&);
 void editfamily();
 void lookupfamily(string[], int&);
-void removefamily(string[], int&);
+void removemem(string[], int&);
+void listfamily(string[], int&);
+
+// Info/Storage manager
+void readin(string[], int&);
+void save(string[], int&);
+void cleaner(string[], int&);
 
 int main() {
-	int input = 0, instore = 0;
+	int input = 0, instore = 1;
 	string familymem[500];
 
 	welcome();
+	readin(familymem, instore);
+	cleaner(familymem, instore);
+	
 	menu(input, familymem, instore);
 
 	exit(0);
 }
 
+void readin(string familymem[], int& instore) {
+	string name;
+	ifstream wishlist;
+
+	wishlist.open("WishList.txt");
+	
+	/*
+	for (int i = 0; i <= 500; i++) {
+			wishlist >> familymem[instore];
+			instore++;
+		}*/
+	
+	
+	while (getline(wishlist, name)) {
+		familymem[instore] = name;
+		instore++;
+	}
+
+	for (int i = 1; i <= instore; i++)
+		if (familymem[instore] == "")
+			instore--;
+	
+	wishlist.close();
+}
+
+void save(string familymem[], int& instore) {
+	ofstream wishlist;
+
+	wishlist.open("WishList.txt");
+
+	for (int familyamount = 1; familyamount <= instore; familyamount++)
+		wishlist << familymem[familyamount] << endl;
+
+	wishlist.close();
+}
 
 void welcome() {
 	cout << "Welcome to the Family Tree List" << endl;
@@ -35,55 +80,127 @@ void welcome() {
 }
 
 void menu(int& choice, string familymem[], int& instore) {
-	while (choice != 4) {
+	while (choice != 5) {
 		cout << "1. Add a member" << endl;
-		cout << "2. Search a member" << endl;
+		cout << "2. Select a Family Member" << endl;
 		cout << "3. Remove a memeber" << endl;
-		cout << "4. Quit Program" << endl;
+		cout << "4. List Family Memebers" << endl;
+		cout << "5. Quit Program" << endl;
 
 		cin >> choice;
 			switch (choice) {
 			case 1:
 				addfamily(familymem, instore);
+				save(familymem, instore);
 				break;
 			case 2:
 				lookupfamily(familymem, instore);
+				save(familymem, instore);
 				break;
 			case 3:
-				// removemem(familymem, instore);
+				removemem(familymem, instore);
+				save(familymem, instore);
 				break;
 			case 4:
+				listfamily(familymem, instore);
+				save(familymem, instore);
+				break;
+			case 5:
 				exit(0);
+				break;
 			}
 		}
 }
 
 void addfamily(string familymem[], int& instore) {
 	string newfamily, newname;
+	bool exist = 0;
+
+	cleanup();
 
 	cout << "Please enter the new family member name" << endl;
 	cin >> newfamily;
-	instore = 1 + instore;
-	familymem[instore] = newfamily;
-}
 
+	while (exist == 0) {
+		for (int i = 1; i <= instore; i++) {
+			if (familymem[i] == newfamily) {
+				cout << newfamily << " already exist." << endl;
+				break;
+			}
+		}
+		exist = 1;
+
+		/*
+		if(exist == 0){
+			cout << "Please put in another family memeber name: ";
+			cin >> newfamily;
+		}
+		*/
+	}
+	
+	if (exist == 1) {
+		instore = 1 + instore;
+		familymem[instore] = newfamily;
+	}
+}
 
 void lookupfamily(string familymem[], int& instore) {
 	string lookfor;
 
-	system("CLS");
-	
+	cleanup();
+
 	cout << "Please put in the family member name you're looking for; ";
 	cin >> lookfor;
-
-	for (int i = 0; i <= 500; i++) {
-		if (familymem[i] == lookfor) {
-			cout << lookfor << " is the " << i << " member of the family" << endl;
-			break;
+		for (int i = 1; i <= instore; i++) {
+			if (familymem[i] == lookfor) {
+				cout << "-------------------------------" << endl;
+				cout << lookfor << " is the " << i << " member of the family" << endl;
+				break;
+			}
 		}
-	}
+}
+
+void listfamily(string familymem[], int& instore) {
+	string lookfor;
+
+	cleanup();
+
+	cout << "Here's a list of your family" << endl;
+	cout << "-------------------------------" << endl;
+
+		for (int i = 1; i <= instore; i++) {
+			cout << i << ". " << familymem[i] << endl;
+			}
+
+	cout << "-------------------------------" << endl;
 }
 
 void removemem(string familymem[], int& instore) {
+	int remove, family;
+	cleanup();
 
+	cout << "Who do you want to remove from the family" << endl;
+	cout << "-------------------------------" << endl;
+
+	for (int family = 1; family <= instore; family++) {
+		cout << family << ". " << familymem[family] << endl;
+	}
+	cout << "-------------------------------" << endl;
+
+	cin >> remove;
+
+		familymem[remove] = "";
+
+		for (int family = 1; family <= instore; family++) {
+			familymem[remove] = familymem[remove + 1];
+		}
+		
+}
+
+void cleaner(string familymem[], int& instore) {
+	struct Node* list;
+
+}
+void cleanup() {
+	system("CLS");
 }
